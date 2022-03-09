586a221195eb29e61f37fa1ab01848a7a54447ce
George <georgesucuzhanay100@gmail.com>
2022-03-08 19:43:28 -0500

    completed redux lab using reducers

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux'
 import { createStore} from 'redux'
+import birdApp from './store/birds/birds';
 
-const store = createStore(() => ({
-  birds: [
-    {
-      name: 'robin',
-      views: 1
-    }
-  ]
-}))
+const store = createStore(birdApp);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
